0b42d3fcd6a3914494cffa7a9ebcfb3c376eabf4
Kalkunov Aman <mrkalkunov@yandex.ru>
2021-02-10 10:40:09 +0400

    fix: task complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
-exports.min = function min (array) {
+exports.min = function min(array) {
+  if (array !== undefined && array.length !== 0) {
+    let number = Math.min(...array);
+    return number;
+  }
   return 0;
 }
 
-exports.max = function max (array) {
+exports.max = function max(array) {
+  if (array !== undefined && array.length !== 0) {
+    let number = Math.max(...array);
+    return number;
+  }
   return 0;
 }
 
-exports.avg = function avg (array) {
+exports.avg = function avg(array) {
+  if (array !== undefined && array.length !== 0) {
+    let number = array.reduce((a, b) => {
+      return a + b;
+    }) / array.length;
+    return number
+  }
   return 0;
 }
